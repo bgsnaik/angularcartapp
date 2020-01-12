8182f7320702a44a3a9d4673714cb55f4ce6ae58
Santhosh <emailtonaik6@gmail.com>
2020-01-13 00:08:40 +0530

    changes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FilterComponent } from './filter/filter.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
+  import { from } from 'rxjs';
 
-
-const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomepageComponent },
+    { path: 'home', component: HomepageComponent },
+    { path: 'cart', component: CartComponentComponent },
+    { path: 'filter', component: FilterComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
+import { HomepageComponent } from './homepage/homepage.component';
+import { FooterComponentComponent } from './footer-component/footer-component.component';
+import { SearchComponentComponent } from './search-component/search-component.component';
+import { CartComponentComponent } from './cart-component/cart-component.component';
+import { FilterComponent } from './filter/filter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomepageComponent,
+    FooterComponentComponent,
+    SearchComponentComponent,
+    CartComponentComponent,
+    FilterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
